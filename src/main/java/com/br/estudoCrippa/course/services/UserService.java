de22142cdabdcaf9af1eaaad2a20a32937f1fe78
LucasCrippa <lucas_crippa@hotmail.com>
2025-02-18 12:17:23 -0300

    Exception handling - update

--- src/main/java/com/br/estudoCrippa/course/services/UserService.java
+++ src/main/java/com/br/estudoCrippa/course/services/UserService.java
@@ -5,13 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.br.estudoCrippa.course.entities.User;
 import com.br.estudoCrippa.course.repositories.UserRepository;
 import com.br.estudoCrippa.course.services.exceptions.DatabaseException;
 import com.br.estudoCrippa.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -46,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 
